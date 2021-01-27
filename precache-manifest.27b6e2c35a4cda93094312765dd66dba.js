90569bf69b42c40c1fd3fe45dbb07edc71cec878
TaraTorpeedo <tarita@hotmail.fi>
2021-01-27 23:34:51 +0200

    Updates

---
+++ precache-manifest.27b6e2c35a4cda93094312765dd66dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1369880cb1eba675ccc0d50987139a",
+    "url": "/taritakar/index.html"
+  },
+  {
+    "revision": "724b041440c2ea0b421d",
+    "url": "/taritakar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a17bf967ac26a8d3feeb",
+    "url": "/taritakar/static/js/2.5f887cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taritakar/static/js/2.5f887cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724b041440c2ea0b421d",
+    "url": "/taritakar/static/js/main.c973d2fe.chunk.js"
+  },
+  {
+    "revision": "a0b0b944ca0e05419ed4",
+    "url": "/taritakar/static/js/runtime-main.e19a2e98.js"
+  }
+]);
